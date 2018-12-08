4b0300dc3b9a23df2d227b851e5cbcced16ce406
Insane Shadow <insaneshadowzaman@gmail.com>
2018-12-08 19:18:17 +0600

    modified for final project

--- converttoutf.py
+++ converttoutf.py
@@ -1,6 +1,9 @@
 import io
 import json
-with io.open("results.json",'w', encoding="utf-8") as outfile:
-    infile = io.open("prothom.json", 'r', encoding="utf-8")
-    js = json.load(infile)
-    outfile.write(json.dumps(js, ensure_ascii=False, indent=4))
+
+class JSONUtf:
+    def start(self):
+        with io.open("results.json", 'w', encoding="utf-8") as outfile:
+            infile = io.open("prothomaloscraping/prothom.json", 'r', encoding="utf-8")
+            js = json.load(infile)
+            outfile.write(json.dumps(js, ensure_ascii=False, indent=4))
--- prothomalo/spiders/archive_getter.py
+++ prothomalo/spiders/archive_getter.py
@@ -14,39 +14,38 @@ class ProthomSpider(scrapy.Spider):
         'https://www.prothomalo.com/archive/2017-01-31'
     ]
 
-    def __init__(self):
+    def __init__(self, date):
         self.driver = webdriver.Firefox()
+        ProthomSpider.start_urls = [
+            'https://www.prothomalo.com/archive/' + date
+        ]
 
     def parse(self, response):
-        for div in response.xpath("//div[@class='listing']/div"):   # Xpath language
-            yield {   # dictionary
+        for div in response.xpath("//div[@class='listing']/div"):  # Xpath language
+            yield {  # dictionary
                 'title-index': div.xpath(".//h2/span/text()").extract_first(),
                 'link-index': div.xpath("./a/@href").extract_first()
             }
             for link in div.xpath("./a"):
                 yield response.follow(link, callback=self.parse_content)
-    
+
     def parse_content(self, response):
 
         browser = self.driver.get(response.url)
 
-        # nn = self.driver.find_element_by_xpath("//div[@class='comment_portion']/p")
+        delay = 2  # seconds
 
-        delay = 5 # seconds
-        comment = None
-
-        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);") #javascript execute in browser
-        time.sleep(2) #sleep
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")  # javascript execute in browser
+        time.sleep(delay)  # sleep
         # comment = self.driver.find_element_by_css_selector("//div[@class='comment_portion']//p")
         try:
-            comment = self.driver.find_elements_by_css_selector("ul.comments_holder_ul>li p")  # css selector to get array
+            comment = self.driver.find_elements_by_css_selector(
+                "ul.comments_holder_ul>li p")  # css selector to get array
         except NoSuchElementException:
-            comment = None # if no comments
-        comments = [ c.text for c in comment ] # get only text from the gathered comments
-        # comment = self.driver.find_element_by_xpath("//div[@class='comment_portion']//p/text()")
-        # comment = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH, "//div[@id='comments']//p")))
+            comment = None  # if no comments
+        comments = [c.text for c in comment]  # get only text from the gathered comments
 
-        yield { # return dictionary that scrapy will convert to json and write in output file.
+        yield {  # return dictionary that scrapy will convert to json and write in output file.
             'title-page': response.xpath("//h1/text()").extract_first(),
             'content': response.xpath("//article/div//p/text()").extract(),
             'comment': comments
